bb453c5ba9dd829b6a52a2a19d02f3ce0a1aa41c
Edward Thomas <edward_thomas@outlook.com>
2022-03-25 16:20:16 +0000

    added key exchange

--- server.py
+++ server.py
@@ -288,9 +288,11 @@ class Battle:
                 self.p2socket = player
                 p2connected = True
                 print(f"{player2} connected")
-                
+
         SERVER.send("IP", self.p1socket, self.player1.key, self.player2.socket.getpeername()[0], self.player1first)
+        self.p1socket.send(self.player2.key.save_pkcs1("DER"))
         SERVER.send("IP", self.p2socket, self.player2.key, self.player1.socket.getpeername()[0], not self.player1first)
+        self.p2socket.send(self.player1.key.save_pkcs1("DER"))
         print(f"Battle between {player1.username} and {player2.username} initialised!")
                 
     def Run(self):
