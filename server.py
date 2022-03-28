7c3fb1b1a79d8e30c815edaf560d943e83d5d5dd
Edward Thomas Laptop <edward_thomas@outlook.com>
2022-03-28 11:57:53 +0100

    changed dynamic server ip

--- server.py
+++ server.py
@@ -369,7 +369,9 @@ class Server: #Class containing server methods and attributes
 
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Socket specifying using the tcp/ip protocol
         self.__socket.settimeout(1)
-        self.__host = socket.gethostbyname(socket.gethostname()) #Server ip address
+        temp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        temp.connect(("8.8.8.8", 80))
+        self.__host = temp.getsockname()[0] #Server ip address
         self.__port = 11034 #Server port
         self.__pubkey, self.__privkey = rsa.newkeys(2048)
         self.__CountryNames = [] #type: list[str]
