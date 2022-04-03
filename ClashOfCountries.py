66161c05c3a111af54fc388a114c0320fc2450e9
Edward Thomas Laptop <edward_thomas@outlook.com>
2022-04-03 14:35:48 +0100

    changed ip

--- ClashOfCountries.py
+++ ClashOfCountries.py
@@ -1020,7 +1020,7 @@ class Connection:
 
     def __init__(self):
         t = Thread(target=LoadScreen, args=["Connecting to server!"])
-        self.HOST = "127.0.1.1"
+        self.HOST = "192.168.1.64"
         self.PORT = 11034
         self.SOCK = s.socket(s.AF_INET, s.SOCK_STREAM)
         self.regularSock = self.SOCK
